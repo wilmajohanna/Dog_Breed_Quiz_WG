b03908d3115454cb875c1c1ed03402c26c2688b0
Wilma Gotthardsson <93529076+wilmajohanna@users.noreply.github.com>
2023-06-02 11:26:01 +0200

    fetch function

--- js/server_connection.js
+++ js/server_connection.js
@@ -1,15 +1,27 @@
 "use strict"
 
-async function fetch_function(request, object) {
-    return await fetch(request, object);
+refresh_page();
 
+async function fetch_function(request) {
+    let response = await fetch(request);
+    return response;
+};
+
+function refresh_page() {
+    if (window.localStorage.getItem(".username > input") !== null) {
+        activate_quiz();
+
+    } else {
+        document.querySelector("#css_file").href = "./css/login_register.css"
+    }
 };
 
 function loading_alert(alert) {
 
     let alert_box = document.createElement("div");
     alert_box.id = "white_box";
-    document.querySelector("body").prepend(alert_box); //prepend = hamnar längst upp
+    /* document.querySelector("body").prepend(alert_box); */
+    document.body.prepend(alert_box); //prepend = hamnar längst upp
     alert_box.innerHTML = `<div class="loading_text">${alert}</div>`;
 }
 
@@ -17,12 +29,11 @@ function remove_loading_alert(alert) {
     document.querySelector("#white_box").remove();
 }
 
-function teapot_alert(alert) {
+function alert_message(alert) {
     let white_background = document.createElement("div");
     white_background.id = "white_box";
     document.body.prepend(white_background);
 
-    white_background.innerHTML = `<div class="teapot_popup">${alert}<button>CLOSE</button></div>`;
-
-    document.querySelector(".teapot_pot > button").addEventListener("click", () => white_background.remove());
+    white_background.innerHTML = `<div class="alert_popup">${alert}<button>CLOSE</button></div>`;
+    document.querySelector(".alert_popup > button").addEventListener("click", () => white_background.remove());
 };
