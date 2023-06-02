b3df82df4ef398313d0921e36ec11c6284e7792a
Wilma Gotthardsson <93529076+wilmajohanna@users.noreply.github.com>
2023-06-02 15:44:56 +0200

    quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,20 +1,78 @@
 "use strict"
 
-async function fetch_dog(dog_oject) {
 
-    const dog_request = new Request(`https://dog.ceo/api/breed/${dog_oject.url}/images/random`);
+async function activate_quiz(user_name) {
 
-    try {
+    css_switch();
 
-        let response = await fetch(dog_request);
-        let resource = await response.json();
-        document.querySelector("#dog_img").src = resource.message;
+    let unique_dogs = [];
 
-    } catch (error) {
-        console.log(error);
-    }
+    while (unique_dogs.length < 4) {
+        let alternatives = ALL_BREEDS[Math.floor(Math.random() * ALL_BREEDS.length)];
+
+        if (!unique_dogs.includes(alternatives)) {
+            unique_dogs.push(alternatives);
+        }
+    };
+
+    console.log(user_name);
+    document.querySelector("#name").innerHTML = `
+
+    <p>${user_name}</p>
+    <button id="log_out">log out</button>`;
+
+
+    function log_out() {
+        location.reload();
+        document.querySelector("#css_file").href = "./css/login_register.css"
+    };
+
+    document.querySelector("#log_out").addEventListener("click", log_out);
+
+    const dog_object = unique_dogs[Math.floor(Math.random() * unique_dogs.length)];
+    const dog_request = `https://dog.ceo/api/breed/${dog_object.url}/images/random`;
+    const dog_img = await (await fetch(dog_request)).json();
+
+    document.querySelector("#dog_img").src = `${dog_img.message}`;
+
+    /* ---------------- alternatives ---------------*/
+
+    for (let i = 0; i < unique_dogs.length; i++) {
+        let alt_options = document.createElement("button");
+        alt_options.textContent = unique_dogs[i].name;
+        alt_options.addEventListener("click", response);
+        document.querySelector("#alt_quiz").append(alt_options);
+
+        function response() {
+            if (alt_options.innerText === dog_object.name) {
+
+                let background = document.createElement("div");
+                background.id = "white_box";
+                document.body.prepend(background);
+
+                background.innerHTML = `<div class="correct_answer">CORRECT ANSWER<button>ONE MORE</button></div>`;
+                document.querySelector(".correct_answer > button").addEventListener("click", () => {
+                    background.remove();
+                    document.querySelector("#alt_quiz").innerHTML = "";
+                    activate_quiz(user_name);
+
+                });
+
+            } else {
+
+                let background = document.createElement("div");
+                background.id = "white_box";
+                document.body.prepend(background);
+
+                background.innerHTML = `<div class="wrong_answer">WRONG ANSWER<button>TRY AGAIN</button></div>`;
+                document.querySelector(".wrong_answer > button").addEventListener("click", () => {
+                    background.remove();
+                    document.querySelector("#alt_quiz").innerHTML = "";
+                    activate_quiz(user_name);
+                });
+            };
+        };
+
+    };
 };
 
-
-
-fetch_dog(ALL_BREEDS[]);
