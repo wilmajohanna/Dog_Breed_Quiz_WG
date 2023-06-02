d004a92da1375710373c9257bdcdc27b7446d7ff
Wilma Gotthardsson <93529076+wilmajohanna@users.noreply.github.com>
2023-06-02 15:26:53 +0200

    extra material

--- js/server_connection.js
+++ js/server_connection.js
@@ -1,26 +1,13 @@
 "use strict"
 
-refresh_page();
-
 async function fetch_function(request) {
     let response = await fetch(request);
     return response;
 };
 
-function refresh_page() {
-    if (window.localStorage.getItem(".username > input") !== null) {
-        activate_quiz();
-
-    } else {
-        document.querySelector("#css_file").href = "./css/login_register.css"
-    }
-};
-
 function loading_alert(alert) {
-
     let alert_box = document.createElement("div");
     alert_box.id = "white_box";
-    /* document.querySelector("body").prepend(alert_box); */
     document.body.prepend(alert_box); //prepend = hamnar längst upp
     alert_box.innerHTML = `<div class="loading_text">${alert}</div>`;
 }
@@ -33,7 +20,15 @@ function alert_message(alert) {
     let white_background = document.createElement("div");
     white_background.id = "white_box";
     document.body.prepend(white_background);
-
     white_background.innerHTML = `<div class="alert_popup">${alert}<button>CLOSE</button></div>`;
     document.querySelector(".alert_popup > button").addEventListener("click", () => white_background.remove());
 };
+
+function wrong_user() {
+    document.querySelector(".inspo_text").textContent = "Wrong username or password";
+    document.querySelector(".inspo_text").style.backgroundColor = "lightblue";
+}
+
+function css_switch(event) {
+    document.querySelector("#css_file").href = "./css/quiz.css"
+}
